83982766bba6d433c4e96a4f92f8220aeedeea3f
Onix Piemntel <onix.pimentel@hunter.do>
2022-06-23 17:25:51 -0400

    Router v6

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Navigate, NavLink } from 'react-router-dom';
 import { Routes, Route, Link } from 'react-router-dom';
+import {routes} from './routes';
+
+
 import logo from '../logo.svg';
 import '../App.css'
-import { LazyPage1 } from '../01-lazyload/pages/LazyPage1';
-import { LazyPage2 } from '../01-lazyload/pages/LazyPage2';
-import { LazyPage3 } from '../01-lazyload/pages/LazyPage3';
+
+
 
 export const Navigation = () => {
   return (
@@ -13,23 +15,25 @@ export const Navigation = () => {
             <nav>
                 <img src={logo} className="App-logo" alt="logo" />
                 <ul>
-                    <li>
-                        <NavLink to="/lazy1">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/lazy2">About</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/lazy3">Users</NavLink>
-                    </li>
+                    {
+                        routes.map( route => (
+                        <li key={route.to}>
+                            <NavLink to={route.to}>{ route.name }</NavLink>
+                        </li>
+                        ))
+                    }
+
+                    
                 </ul>
             </nav>
 
             <Routes>
-                <Route path='lazy1' element={<LazyPage1/>} />
-                <Route path='lazy2' element={<LazyPage2/>} />
-                <Route path='lazy3' element={<LazyPage3/>} />
-                <Route path='/*'    element={<Navigate to="/lazy1" replace />} />
+                    {
+                        routes.map( route => (
+                            <Route key={route.to} path={route.path} element={<route.Component/>} />
+                        ))
+                    }
+                <Route path='/*'    element={<Navigate to={routes[0].to} replace />} />
             </Routes>
 
         </div>  
